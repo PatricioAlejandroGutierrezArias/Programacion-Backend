ff99b049685b5058e6bf8d90aa87b65bd2d106dc
PatricioGutierrezArias <patricio.gutierrez@inacapmail.cl>
2023-10-25 20:00:21 -0300

    251020232000

--- Proyecto_3/Proyecto_3/gestion_app/views.py
+++ Proyecto_3/Proyecto_3/gestion_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from gestion_app.models import Productos
 
 # Create your views here.
 
 def busqueda(request):
     return render(request, "busqueda.html")
+
+def busca(request):
+    if request.GET ["prd"]:
+        produc = request.GET["prd"]
+        produ = Productos.objects.filter(nombre__icontains=produc)
+        return render(request, "resultado.html", {"productos":produ, "query":produc})
+    
+    else: 
+        mensaje = "No has introducido nada" 
+        return render(request, "resultado.html", {"query":mensaje})
+        
+    
