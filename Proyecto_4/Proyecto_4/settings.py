3cb38ae9a024fc54206cf99ba548285971f02b07
PatricioGutierrezArias <patricio.gutierrez@inacapmail.cl>
2023-10-26 11:39:44 -0300

    261020231139

--- Proyecto_4/Proyecto_4/settings.py
+++ Proyecto_4/Proyecto_4/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_ap77',
-        'user': 'root',
+        'USER': 'root',
         'PASSWORD': 'root'
     }
 }
