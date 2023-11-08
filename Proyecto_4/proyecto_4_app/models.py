c87887abefb06356f0ee56f580520c4628055434
PatricioGutierrezArias <patricio.gutierrez@inacapmail.cl>
2023-11-08 20:08:50 -0300

    0811232008

--- Proyecto_4/proyecto_4_app/models.py
+++ Proyecto_4/proyecto_4_app/models.py
@@ -6,3 +6,5 @@ class Empleados(models.Model):
     nombre = models.CharField(max_length=50)
     email = models.EmailField()
     fono = models.IntegerField()
+    class Meta:
+        db_table = 'empleados'
--- Proyecto_4/proyecto_4_app/views.py
+++ Proyecto_4/proyecto_4_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from proyecto_4_app.models import Empleados
 
 # Create your views here.
+def listaempleados(request):
+    emplea = Empleados.objects.all()
+    data = {'emple': emplea}
+    return render(request, 'empleados.html',data)
