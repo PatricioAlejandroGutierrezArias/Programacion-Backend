f626a5ad5965e996ca6e389b4609485869dddd5c
PatricioGutierrezArias <patricio.gutierrez@inacapmail.cl>
2023-11-15 21:15:20 -0300

    151120232115

--- proyecto_6/proyecto_6/proyecto_6_app/models.py
+++ proyecto_6/proyecto_6/proyecto_6_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Proyecto(models.Model):
+    FechaInicido = models.DateField() 
+    FechaTermino = models.DateField() 
+    nombre = models.CharField(max_length=50)
+    responsable = models.CharField(max_length=50)
+    prioridad = models.IntegerField()
+    
+    class Meta:
+        db_table = 'proyecto'
